96a0df1433fb0bed4699730bba35f570af2395f5
mytharcher <mytharcher@gmail.com>
2012-05-21 09:22:08 +0800

    Test references plugin (revert).

--- _plugins/references.rb
+++ _plugins/references.rb
@@ -1,4 +1,3 @@
-
 # references.rb: jekyll markdown references plugin
 #
 # add this file to your _plugins directory (create it if needed)
@@ -10,28 +9,33 @@
 # you can now reference these links in any markdown file
 # for example:
 #   You should [check out JSShaper][jsshaper]
+# 
+# 2012-05-12 Update by [mytharcher](http://github.com/mytharcher)
+# - Move the module to the place under "MarkdownConverter".
+# - Overwrite the "convert" method to insert a preprocess hook to append references content.
+# - Add prefix and suffix new line to make sure that the references should be a separated markdown block.
 
 module Jekyll
-  module Convertible
-    alias old_read_yaml read_yaml
+
+  class MarkdownConverter
+
+    alias old_convert convert
+
     @@refs_content = nil
 
-    def read_yaml(base, name)
-      # loads file, sets @content, @data
-      old_read_yaml(base, name)
-
-      # only alter markdown files
-      return unless converter.instance_of? MarkdownConverter
-
+    def get_references()
       # read and cache content of _references.md
       if @@refs_content.nil?
-        refs_path = File.join(site.source, "_references.md")
+        refs_path = File.join(Jekyll::DEFAULTS['source'], "_references.md")
         @@refs_content = if File.exist?(refs_path) then File.read(refs_path) 
                          else "" end
       end
+      @@refs_content
+    end
 
-      # append content of _references.md, whatever it is
-      @content += @@refs_content
+    def convert(content)
+      # To make sure that the references should be a separated markdown block.
+      old_convert(content + "\n\n" + get_references + "\n\n")
     end
   end
 end
